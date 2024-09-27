e85ec5171d91e0f02b0554531dc0a0cdbf954908
Antonio Segalini <132697545+AntSegalini@users.noreply.github.com>
2024-09-27 18:12:56 +0200

    Add files via upload

--- main_3D.py
+++ main_3D.py
@@ -11,16 +11,16 @@ LY, NY= 500, 64     # Parameters for the wind energy simulation.
 LZ, NZ= 500, 64     # Parameters for the wind energy simulation.
 
 # turbine locations
-X_T=[300]
-Y_T=[0]
-Z_T=[0]
-R_T=[50]
-C_T=[0.8]    
+X_T=[300,600,750]
+Y_T=[0,100,-100]
+Z_T=[0,0,0]
+R_T=[50,50,50]
+C_T=[0.8,0.8,0.8]    
 delta=10
 
 t_max=LX/Uinf
 iter_saving=100
-start_from=0
+start_from=500
 CFL_max=0.2
 
 fileName='simulation'
@@ -54,9 +54,9 @@ def main():
         print(i,' t=',np.round(t,1),' dt=',np.round(sim.dt,3),' MAX DIV=',np.round(np.max(np.abs(sim.DIVERGENCE(sim.u,sim.v,sim.w))),4))
         
         if np.mod(i,iter_saving)==0:
-            savemat(fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
+            savemat(fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_w':sim.x_w,'y_w':sim.y_w,'z_w':sim.z_w,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
 
-    savemat(fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
+    savemat(fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_w':sim.x_w,'y_w':sim.y_w,'z_w':sim.z_w,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
 
     print('elapsed time:',time.time()-t1)
 
