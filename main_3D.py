2daf697e739aa590bb5e17b4cac6df8f902d1eae
Vikranth Nayakanti <vikna520@user.uu.se>
2024-11-27 16:05:26 +0100

    OPS: Added simulation results

--- main_3D.py
+++ main_3D.py
@@ -20,9 +20,10 @@ delta=10
 
 t_max=LX/Uinf
 iter_saving=100
-start_from=500
+start_from=0
 CFL_max=0.2
 
+path = 'sim_res/'
 fileName='simulation'
 
 #########################################################################################
@@ -34,7 +35,7 @@ def main():
 
     t,i=0,0
     if start_from>0:
-        Q=loadmat(fileName+'_'+str(start_from)+'.mat')
+        Q=loadmat(path+fileName+'_'+str(start_from)+'.mat')
         sim.set_uvwp(Q['u'],Q['v'],Q['w'],Q['p'])
         t=np.squeeze(Q['t'])
         i=start_from
@@ -54,9 +55,9 @@ def main():
         print(i,' t=',np.round(t,1),' dt=',np.round(sim.dt,3),' MAX DIV=',np.round(np.max(np.abs(sim.DIVERGENCE(sim.u,sim.v,sim.w))),4))
         
         if np.mod(i,iter_saving)==0:
-            savemat(fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_w':sim.x_w,'y_w':sim.y_w,'z_w':sim.z_w,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
+            savemat(path+fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_w':sim.x_w,'y_w':sim.y_w,'z_w':sim.z_w,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
 
-    savemat(fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_w':sim.x_w,'y_w':sim.y_w,'z_w':sim.z_w,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
+    savemat(path+fileName+'_'+str(i)+'.mat',{'u':sim.u,'v':sim.v,'w':sim.w,'p':sim.p,'t':t,'x_u':sim.x_u,'y_u':sim.y_u,'z_u':sim.z_u,'x_v':sim.x_v,'y_v':sim.y_v,'z_v':sim.z_v,'x_w':sim.x_w,'y_w':sim.y_w,'z_w':sim.z_w,'x_p':sim.x_p,'y_p':sim.y_p,'z_p':sim.z_p})
 
     print('elapsed time:',time.time()-t1)
 
